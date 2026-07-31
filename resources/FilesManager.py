c876b9154dfedf1cbc04058ce00a89a20b9cb4a7
Marcos Farias [ Zallpy ] <marcospaulo_farias@outlook.com>
2026-07-31 15:13:51 -0300

    feat(filesmanager): add function to verify if a file exists

--- resources/FilesManager.py
+++ resources/FilesManager.py
@@ -1,17 +1,18 @@
 from os import listdir, path
 import os
 import shutil
+from loguru import logger
 from resources.PathManager import PathManager
 from resources.PrintAutomation import PrintAutomation
 
 class FilesManager:
-    def __init__(self) -> None:
+    def __init__(self, process_id: str, process_type: str, process_machine: str) -> None:
         """Classe para gerenciamento de arquivos.
         
         :returns None:
         """
         self.pathmanager = PathManager()
-        self.printautomation = PrintAutomation()
+        self.printautomation = PrintAutomation(process_id=process_id, process_type=process_type, process_machine=process_machine)
 
     def _list_files(self, directory: str) -> list|RuntimeError:
         """Lista todos os arquivos em um diretório específico.
@@ -21,9 +22,10 @@ class FilesManager:
         :raises RunTimeError: caso ocorra algum erro ao listar os arquivos, ex: diretório inexistente."""
         try:
             return [os.path.join(directory, file_name) for file_name in listdir(directory)]
-        except Exception as e:
-            self.printautomation.print_error(f"Erro ao tentar listar arquivos no diretório {directory}: {e}")
-            raise RuntimeError(f"Erro ao tentar listar arquivos no diretório {directory}: {e}")
+        except Exception as error_x:
+            self.printautomation.print_error()
+            logger.critical(f'Erro ao tentar listar os arquivos em "{directory}".\nErro: {error_x}.')
+            raise RuntimeError(f'Erro ao tentar listar os arquivos em "{directory}".\nErro: {error_x}.') from error_x
 
     def _rm_file(self, file_to_remove: str) -> None:
         """Remove um arquivo específico.
@@ -39,9 +41,10 @@ class FilesManager:
                 os.remove(file_to_remove)
         except FileNotFoundError:
             ...  # Se o arquivo não existir, não faz nada
-        except Exception as e:
-            self.printautomation.print_error(f"Erro ao tentar remover o arquivo {file_to_remove}: {e}")
-            raise RuntimeError(f"Erro ao tentar remover o arquivo {file_to_remove}: {e}")
+        except Exception as error_x:
+            self.printautomation.print_error()
+            logger.critical(f'Erro ao tentar remover o arquivo "{file_to_remove}".\nErro: {error_x}.')
+            raise RuntimeError(f'Erro ao tentar remover o arquivo "{file_to_remove}".\nErro: {error_x}.') from error_x
         
     def clean_paths(self, paths_to_clean: list) -> None|RuntimeError:
         """Limpa os diretórios especificados, removendo todos os arquivos dentro deles.
@@ -55,6 +58,20 @@ class FilesManager:
                     full_path = os.path.join(dir_path, file_name)
                     if os.path.isfile(full_path):
                         self._rm_file(full_path)
-            except Exception as e:
-                self.printautomation.print_error(f"Erro ao tentar limpar o diretório {dir_path}: {e}")
-                raise RuntimeError(f"Erro ao tentar limpar o diretório {dir_path}: {e}")
+            except Exception as error_x:
+                self.printautomation.print_error()
+                logger.critical(f'Erro ao tentar limpar o diretório "{dir_path}".\nErro: {error_x}.')
+                raise RuntimeError(f'Erro ao tentar limpar o diretório "{dir_path}".\nErro: {error_x}.') from error_x
+
+    def verify_exists_file(self, file: str) -> bool:
+        """Função para verificar se um arquivo existe.
+        
+        :param file: arquivo para verificar a existência, ex: "{caminho}+{arquivo.extensão}".
+        :returns True: se or arquivo existir.
+        :returns False: se o arquivo não existir."""
+        try:
+            return path.exists(file)
+        except Exception as error_x:
+            logger.critical(f'Erro ao verificar se o arquivo "{file}" existe.\nErro: {error_x}.')
+            raise FileExistsError(f'Erro ao verificar se o arquivo "{file}" existe.\nErro: {error_x}.') from error_x
+            
